bb6887209d36eb81f6f5dab842603e24e8b3666d
jpcp0614 <jpcp0614@gmail.com>
2023-09-27 16:30:30 -0300

    Usando array no state

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
@@ -8,62 +7,38 @@ class App extends Component {
 	// 	this.handlePClick = this.handlePClick.bind(this);
 
 	state = {
-		name: 'Katy Smith',
-		counter: 0,
+		posts: [
+			{
+				id: 1,
+				title: 'Título 1',
+				body: 'Corpo 1',
+			},
+			{
+				id: 2,
+				title: 'Título 2',
+				body: 'Corpo 2',
+			},
+			{
+				id: 3,
+				title: 'Título 3',
+				body: 'Corpo 3',
+			},
+		],
 	};
 
-	handlePClick = () => {
-		this.setState({ name: 'John Doe' });
-	};
-
-	handleAClick = (e) => {
-		e.preventDefault();
-		const { counter } = this.state;
-		this.setState({ counter: counter + 1 });
-	};
 	render() {
-		const { name, counter } = this.state;
+		const { posts } = this.state;
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p onClick={this.handlePClick}>
-						{name} - {counter}
-					</p>
-					<a
-						onClick={this.handleAClick}
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Este é o link
-					</a>
-				</header>
+				{posts.map((post) => (
+					<div key={post.id}>
+						<h1>{post.title}</h1>
+						<p>{post.body}</p>
+					</div>
+				))}
 			</div>
 		);
 	}
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
