7f45a66b29638a77109579b4a229ae607a6efbca
jpcp0614 <jpcp0614@gmail.com>
2023-09-27 16:15:47 -0300

    Classe com estado e classe field

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -3,26 +3,46 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
+	// constructor(props) {
+	// 	super(props);
+	// 	this.handlePClick = this.handlePClick.bind(this);
+
+	state = {
+		name: 'Katy Smith',
+		counter: 0,
+	};
+
+	handlePClick = () => {
+		this.setState({ name: 'John Doe' });
+	};
+
+	handleAClick = (e) => {
+		e.preventDefault();
+		const { counter } = this.state;
+		this.setState({ counter: counter + 1 });
+	};
+	render() {
+		const { name, counter } = this.state;
+		return (
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
+					<p onClick={this.handlePClick}>
+						{name} - {counter}
 					</p>
 					<a
+						onClick={this.handleAClick}
 						className="App-link"
 						href="https://reactjs.org"
 						target="_blank"
 						rel="noopener noreferrer"
 					>
-						Learn React
+						Este é o link
 					</a>
 				</header>
 			</div>
 		);
-  };
+	}
 }
 
 // function App() {
