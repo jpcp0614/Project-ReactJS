144364a26cdc289cbeb197f9432c5215282d320d
jpcp0614 <jpcp0614@gmail.com>
2023-09-30 12:43:47 -0300

    Posts na Home

--- project-1/src/tests/Home.spec.jsx
+++ project-1/src/tests/Home.spec.jsx
@@ -1,4 +1,4 @@
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { fireEvent, render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import Home from '../templates/Home';
@@ -79,6 +79,34 @@ describe('<Home />', () => {
 
     const button = screen.getByRole('button', { name: /mais posts/i });
     expect(button).toBeInTheDocument();
+  });
+
+  it('should search for posts', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não tem posts');
+
+    expect.assertions(11);
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/digite aqui/i);
+    expect(screen.getByRole('heading', { name: 'title1 - 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2 - 2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3 - 3' })).not.toBeInTheDocument();
+
+    fireEvent.change(search, { target: { value: 'title1' } });
+    expect(screen.getByRole('heading', { name: 'title1 - 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Search Input: title1' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title2 - 2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3 - 3' })).not.toBeInTheDocument();
+
+    fireEvent.change(search, { target: { value: '' } });
+    expect(screen.getByRole('heading', { name: 'title1 - 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2 - 2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3 - 3' })).not.toBeInTheDocument();
+
+    fireEvent.change(search, { target: { value: 'post does not exist' } });
+    expect(screen.getByText('Não tem posts')).toBeInTheDocument();
 
     screen.debug();
   });
